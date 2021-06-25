03d67eb6926fa70deb1b9a04a7dcef6314556ab0
scottydphillips <scotty.d.phillips@gmail.com>
2021-06-24 21:39:23 -0400

    IT'S ALIVEgit add -Agit add -A

--- script.js
+++ script.js
@@ -1,146 +1,48 @@
 var currentDay = document.getElementById("currentDay")
 var saveBtn = $(".saveBtn");
-var input = $(".input");
+var input = $(".input")
 var now = Number(moment().format('k'));
-var eight = $("8am");
-var nine = $("9am");
-var ten = $('10am');
-var eleven = $('11am');
-var noon = $('12pm');
-var one = $("1pm");
-var two = $("2pm");
-var three = $("3pm");
-var four = $("4pm");
-var five = $("5pm");
+var eight = $("#8am")
+var nine = $("#9am")
+var ten = $("#10am")
+var eleven = $("#11am")
+var noon = $("#12pm")
+var one = $("#1pm")
+var two = $("#2pm")
+var three = $("#3pm")
+var four = $("#4pm")
+var five = $("#5pm")
+var placeholder = [eight, nine, ten, eleven, noon, one, two, three, four, five]
+var taskArray = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 
+// Handles the display of the date in the header
 var displayDate = function() {
 currentDay.textContent = moment().format("[Today is ] MMMM Do YYYY")
 };
 
 displayDate();
 
-saveBtn.on("submit", function(event) {
+// Handles saving the data to local storage on clicking the save button
+saveBtn.on("click", function(event) {
 	event.preventDefault();
-	localStorage.setItem("task", input.innerHTML);
+	console.log("Banana")
+	for(i=0; i<placeholder.length; i++) {
+		localStorage.setItem(taskArray[i], JSON.stringify(placeholder[i].val()));
+	}
 });
 
-var sameTime = function() {
-	if(moment().isSame("11", "hour")) {
-		console.log("yes it's the same")
-	} else {
-		console.log("no it's not the same")
-	}
+// Retrieves the data from local storage upon page refresh
+for (q=0; q<placeholder.length; q++) {
+	placeholder[q].val(JSON.parse(localStorage.getItem(taskArray[q])))
 }
 
-sameTime()
-
-var eightColors = function() {
-	if(moment().isSame("8", "hour")) {
-		eight.addClass(".present");
-	} else if (moment().isAfter("8", "hour")) {
-		eight.addClass(".past");
+// Handles the color changes based on current time
+for (x=0; x< placeholder.length; x++) {
+	if (now === (x + 8)){
+		placeholder[x].addClass("present")
+	} else if (now > (x + 8)) {
+		placeholder[x].addClass("past")
 	} else {
-		eight.addClass(".future");
-	};
+		placeholder[x].addClass("future")
+	}
 };
-
-var nineColors = function() {
-	if(moment().isSame("9", "hour")) {
-		nine.addClass(".present");
-	} else if (moment().isAfter("9", "hour")) {
-		nine.addClass(".past");
-	} else {
-		nine.addClass(".future");
-	};
-};
-
-var tenColors = function() {
-	if(moment().isSame("10", "hour")) {
-		ten.addClass(".present");
-	} else if (moment().isAfter("10", "hour")) {
-		ten.addClass(".past");
-	} else {
-		ten.addClass(".future");
-	};
-};
-
-var elevenColors = function() {
-	if(moment().isSame("11", "hour")) {
-		eleven.addClass(".present");
-	} else if (moment().isAfter("11", "hour")) {
-		eleven.addClass(".past");
-	} else {
-		eleven.addClass(".future");
-	};
-};
-
-var noonColors = function() {
-	if(moment().isSame("12", "hour")) {
-		noon.addClass(".present");
-	} else if (moment().isAfter("12", "hour")) {
-		noon.addClass(".past");
-	} else {
-		noon.addClass(".future");
-	};
-};
-
-var oneColors = function() {
-	if(moment().isSame("1", "hour")) {
-		one.addClass(".present");
-	} else if (moment().isAfter("1", "hour")) {
-		one.addClass(".past");
-	} else {
-		one.addClass(".future");
-	};
-};
-
-var twoColors = function() {
-	if(moment().isSame("2", "hour")) {
-		two.addClass(".present");
-	} else if (moment().isAfter("2", "hour")) {
-		two.addClass(".past");
-	} else {
-		two.addClass(".future");
-	};
-};
-
-var threeColors = function() {
-	if(moment().isSame("3", "hour")) {
-		three.addClass(".present");
-	} else if (moment().isAfter("3", "hour")) {
-		three.addClass(".past");
-	} else {
-		three.addClass(".future");
-	};
-};
-
-var fourColors = function() {
-	if(moment().isSame("4", "hour")) {
-		four.addClass(".present");
-	} else if (moment().isAfter("4", "hour")) {
-		four.addClass(".past");
-	} else {
-		four.addClass(".future");
-	};
-};
-
-var fiveColors = function() {
-	if(moment().isSame("5", "hour")) {
-		five.addClass(".present");
-	} else if (moment().isAfter("5", "hour")) {
-		five.addClass(".past");
-	} else {
-		five.addClass(".future");
-	};
-};
-
-eightColors();
-nineColors();
-tenColors();
-elevenColors();
-noonColors();
-oneColors();
-twoColors();
-threeColors();
-fourColors();
-fiveColors();
